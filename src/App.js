ee9d45cb25032bb22cca0a1b1aa4ee5e3e5ac2fd
yaffalhakim1 <yafi.tamfan08@gmail.com>
2021-11-16 11:58:44 +0700

    modul 6 pwa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from
+"react-router-dom";
+// Pages
+import LandingPage from "./pages/LandingPage";
+// Components
+import Header from "./components/Header";
+import "./App.css";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+<div className="App">
+<Header />
+<Router>
+<Switch>
+<Route exact path="/" component={LandingPage} />
+</Switch>
+</Router>
+</div>
+);
 }
-
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+export default function index() {
+return (
+<header>
+<h3>Kelompok xx</h3>
+<p>Modul 6 - PWA 2</p>
+</header>
+);
+}
